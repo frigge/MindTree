d24609989abac06d64191b2dc9ab7f6e042baa1f
frigge <frigge@frigge-Destkop.(none)>
2012-01-09 17:26:28 +0100

    fixed compile error

--- source/graphics/shader_view.cpp
+++ source/graphics/shader_view.cpp
@@ -48,10 +48,8 @@ void Shader_View::keyReleaseEvent(QKeyEvent *event)
 
 void Shader_View::mouseReleaseEvent(QMouseEvent *event)
 {
-    if (event->button() == Qt::MiddleButton)
-        setDragMode(RubberBandDrag);
-    else
-        QGraphicsView::mouseReleaseEvent(event);
+    setDragMode(RubberBandDrag);
+    QGraphicsView::mouseReleaseEvent(event);
 };
 
 void Shader_View::drawBackground(QPainter *painter, const QRectF &rect)
