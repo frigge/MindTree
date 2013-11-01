98c2422d44db95f0db8243dadd86fced730ce22b
frigge <saschafricke@frg-vfx.com>
2013-11-01 13:59:08 +0100

    added material class, which will store information about objects appearance

---
+++ MindTree/source/plugins/datatypes/Object/material.h
@@ -0,0 +1,15 @@
+#ifndef MATERIAL_RES1NWMZ
+
+#define MATERIAL_RES1NWMZ
+
+class Material
+{
+public:
+    Material();
+    virtual ~Material();
+
+private:
+    MindTree::PropertyMap properties;
+};
+
+#endif /* end of include guard: MATERIAL_RES1NWMZ */
