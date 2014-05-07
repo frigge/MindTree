9b5b4059a4fb7a2a1bcd22eeffa5decd7fe34572
frigge <saschafricke@frg-vfx.com>
2014-05-07 20:27:32 +0200

    support for custom guis in the properties editor per nodetype

--- MindTree/source/intern/nodefactory.py
+++ MindTree/source/intern/nodefactory.py
@@ -1,4 +1,13 @@
-import MT
+import MT, PyQt4, properties_editor
+from properties_editor import *
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+class CustomNodeWidget(QWidget):
+    def __init__(self, node, parent=None):
+        QWidget.__init__(self, parent)
+        self.node = node
+        self.editor = parent
 
 class NodeFactory:
     type=""
@@ -6,6 +15,7 @@ class NodeFactory:
 
     insockets=[]
     outsockets=[]
+    customwidget = None
 
     def __init__(self, node):
         node.name = self.label.split(".")[-1]
@@ -20,4 +30,8 @@ class NodeFactory:
         for s in self.outsockets:
             socket = node.addOutSocket(*s)
 
+        if self.customwidget is not None:
+            Editor.customWidgets[self.type] = self.customwidget
+
 MT.pytypes.__dict__["NodeFactory"] = NodeFactory
+MT.pytypes.__dict__["CustomNodeWidget"] = CustomNodeWidget
--- MindTree/source/plugins/utilities/addproperties.py
+++ MindTree/source/plugins/utilities/addproperties.py
@@ -1,4 +1,20 @@
-import MT
+import MT, PyQt4
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+class CustomWidget(MT.pytypes.CustomNodeWidget):
+    def __init__(self, node, parent=None):
+        MT.pytypes.CustomNodeWidget.__init__(self, node, parent)
+
+        self.setLayout(QHBoxLayout())
+        button = QPushButton("Add Property")
+        self.layout().addWidget(button)
+        button.clicked.connect(self.addProperty)
+
+    def addProperty(self):
+        self.node.addInSocket("Property Name", "STRING")
+        self.node.addInSocket("Property VALUE", "VARIABLE")
+        self.editor.updateEditor(self.node)
 
 class AddPropertiesNodeFactory(MT.pytypes.NodeFactory):
     type="ADDPROPERTIES"
@@ -7,6 +23,7 @@ class AddPropertiesNodeFactory(MT.pytypes.NodeFactory):
             ("Property Name", "STRING"),
             ("Property Value", "VARIABLE")]
     outsockets = [("Object(s)", "GROUPDATA")]
+    customwidget = CustomWidget
 
 class FilterObjectsNodeFactory(MT.pytypes.NodeFactory):
     type="FILTEROBJECTS"
