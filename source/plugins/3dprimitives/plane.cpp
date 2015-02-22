4292d2abd4ec62d3fb9f9b9c9e654a262d4e25a0
frigge <saschafricke@frg-vfx.com>
2015-02-22 20:40:36 +0100

    explicitly create normals for plane

--- source/plugins/3dprimitives/plane.cpp
+++ source/plugins/3dprimitives/plane.cpp
@@ -8,9 +8,11 @@ GeoObjectPtr prim3d::createPlane(float scale)
     auto obj = std::make_shared<GeoObject>();
     auto mesh = std::make_shared<MeshData>();
     auto vertices = std::make_shared<VertexList>();
+    auto normals = std::make_shared<VertexList>();
     auto polygons = std::make_shared<PolygonList>();
 
     mesh->setProperty("P", vertices);
+    mesh->setProperty("N", vertices);
     mesh->setProperty("polygon", polygons);
     obj->setData(mesh);
 
@@ -19,6 +21,13 @@ GeoObjectPtr prim3d::createPlane(float scale)
                      vec3(scale, 0, -scale),
                      vec3(scale, 0, scale),
                      vec3(-scale, 0, scale),
+                     });
+
+    normals->insert(normals->begin(), {
+                     vec3(0, 1, 0),
+                     vec3(0, 1, 0),
+                     vec3(0, 1, 0),
+                     vec3(0, 1, 0),
                      });
 
     polygons->push_back(Polygon{3, 2, 1, 0});
