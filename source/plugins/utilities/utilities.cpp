f4ea4c2d3136364aea23eb4d995f7b6d98178f7f
Sascha Fricke <frigge@frg-vfx.com>
2015-03-12 23:29:13 +0100

    fixed set property node

--- source/plugins/utilities/utilities.cpp
+++ source/plugins/utilities/utilities.cpp
@@ -7,12 +7,19 @@ using namespace MindTree;
 BOOST_PYTHON_MODULE(utilities) {
     auto addPropertiesProc = [](DataCache *cache) {
         auto grp = cache->getData(0).getData<std::shared_ptr<Group>>();
-        auto prop_name = cache->getData(1).getData<std::string>();
-        auto prop_value = cache->getData(2);
+        auto new_grp = std::make_shared<Group>(*grp);
 
-        for (auto obj : grp->getMembers()) {
-            obj->setProperty(prop_name, prop_value);
+        int prop_cnt = cache->getNode()->getInSockets().size();
+
+        for(int i = 1; i <= prop_cnt; i += 2) {
+            auto prop_name = cache->getData(i).getData<std::string>();
+            auto prop_value = cache->getData(i + 1);
+
+
+            new_grp->setProperty(prop_name, prop_value);
         }
+
+        cache->pushData(new_grp);
     };
 
     DataCache::addProcessor("GROUPDATA", 
