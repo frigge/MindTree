8989487b06a7b0d8495980b41e7b3e97afa4b4ed
Sascha Fricke <frigge@frg-vfx.com>
2015-04-30 20:36:41 +0200

    lock benchmark

--- source/plugins/render/benchmark.cpp
+++ source/plugins/render/benchmark.cpp
@@ -1,4 +1,5 @@
 #include "iomanip"
+#include "iostream"
 #include "algorithm"
 #include "benchmark.h"
 
@@ -24,11 +25,13 @@ Benchmark::Benchmark(std::string name) :
 
 std::string Benchmark::getName() const
 {
+    std::lock_guard<std::recursive_mutex> lock(_benchmarkLock);
     return _name;
 }
 
 void Benchmark::addBenchmark(std::weak_ptr<Benchmark> benchmark)
 {
+    std::lock_guard<std::recursive_mutex> lock(_benchmarkLock);
     if(benchmark.expired())
         return;
 
@@ -38,12 +41,14 @@ void Benchmark::addBenchmark(std::weak_ptr<Benchmark> benchmark)
 
 double Benchmark::getTime() const
 {
+    std::lock_guard<std::recursive_mutex> lock(_benchmarkLock);
     double t = _time.count() / 1'000.0;
     return t / _num_calls;
 }
 
 void Benchmark::start()
 {
+    std::lock_guard<std::recursive_mutex> lock(_benchmarkLock);
     if(_parent && !_parent->_running) {
         _parent->start();
     }
@@ -53,15 +58,20 @@ void Benchmark::start()
 
 void Benchmark::setCallback(std::function<void(Benchmark *)> cb)
 {
+    std::lock_guard<std::recursive_mutex> lock(_benchmarkLock);
     _callback = cb;
 }
 
 void Benchmark::reset()
 {
+    std::lock_guard<std::recursive_mutex> lock(_benchmarkLock);
+
     _time = std::chrono::microseconds(0);
     _num_calls = 0;
     _running = false;
     _children_called = 0;
+
+    std::cout << "Reset Benchmark: " << getName() << std::endl;
 
     for(auto bench : _benchmarks) {
         if(!bench.expired())
@@ -78,11 +88,13 @@ void Benchmark::reset()
 
 int Benchmark::getNumCalls() const
 {
+    std::lock_guard<std::recursive_mutex> lock(_benchmarkLock);
     return _num_calls;
 }
 
 void Benchmark::end()
 {
+    std::lock_guard<std::recursive_mutex> lock(_benchmarkLock);
     if(!_running)
         return;
 
@@ -94,6 +106,7 @@ void Benchmark::end()
     _running = false;
 
     if(_parent) {
+        std::lock_guard<std::recursive_mutex> lock(_parent->_benchmarkLock);
         _parent->_children_called += 1;
         if(_parent->_children_called == _parent->_benchmarks.size())
             _parent->end();
@@ -103,12 +116,13 @@ void Benchmark::end()
 
 std::ostream& MindTree::operator<<(std::ostream &stream, const MindTree::Benchmark &benchmark)
 {
+    std::lock_guard<std::recursive_mutex> lock(benchmark._benchmarkLock);
     if(benchmark._num_calls == 0)
         return stream;
 
     auto calls = std::to_string(benchmark._num_calls);
     auto name = benchmark._name + "(" + calls + "):";
-    stream << std::setw(40) << std::left << name << std::right << benchmark.getTime() << "ms" << std::endl;
+    stream << std::setw(40) << std::left << name << std::right << std::fixed << std::setprecision(2) << benchmark.getTime() << std::endl;
 
     if(benchmark._benchmarks.size() > 1) {
         for(auto bench : benchmark._benchmarks) {
