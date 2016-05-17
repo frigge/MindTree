3d1b7e6a49282bfc11cd1be9991a7653bbdd6fbb
frigge <saschafricke@frg-vfx.com>
2016-05-17 16:33:56 +0200

    array and struct nodes
    patched arraynode from different branch

---
+++ source/lib/data/nodes/arraynode.cpp
@@ -0,0 +1,43 @@
+#include "data/signal.h"
+#include "arraynode.h"
+
+using namespace MindTree;
+
+ArrayNode::ArrayNode(bool raw)
+{
+    if(raw) return;
+
+    new DoutSocket("Output", "VARIABLE", this);
+    new DinSocket("Input", "VARIABLE", this);
+}
+
+void ArrayNode::setType(SocketType t)
+{
+    if (type_ != "VARIABLE")
+        return;
+}
+
+void ArrayNode::addSocket(DSocket *socket)
+{
+    DNode::addSocket(socket);
+    if(socket->getDir() == DSocket::OUT) return;
+
+    auto cb = Signal::getBoundHandler<DoutSocket*>(socket)
+        .connect("linkChanged", [this, socket](DoutSocket *out) {
+                const auto &insockets = this->getInSockets();
+                if (!out)
+                    this->removeSocket(socket);
+                else if(out == socket->toIn()->getCntdSocket()
+                        && !std::all_of(insockets.begin(),
+                                        insockets.end(),
+                                        [](const auto *in) {
+                                return in->getCntdSocket();
+                            }))
+                    return;
+
+                else
+                    new DinSocket("Input", this->type_, this);
+            });
+
+    callbacks_.push_back(cb);
+}
