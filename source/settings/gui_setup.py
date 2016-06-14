cd3e30c401c5b93be87bc3dc978e94452e670203
frigge <saschafricke@frg-vfx.com>
2016-06-14 15:53:40 +0200

    prepare better viewer position

--- source/settings/gui_setup.py
+++ source/settings/gui_setup.py
@@ -3,3 +3,6 @@ graph = MT.gui.showWindow("NodeGraph")
 propedit = MT.gui.showSplitWindow("PropertiesEditor", graph, "HORIZONTAL", 0.25)
 timeline = MT.gui.showSplitWindow("Timeline", graph, "VERTICAL", 0.1)
 console = MT.gui.showTabbedWindow("Console", timeline)
+
+MT.gui.__dict__["viewerPosition"] = graph
+MT.gui.__dict__["viewerOrientation"] = "HORIZONTAL"
