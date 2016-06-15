9e20dcbcbee75ade107600f19564bf47c377a496
frigge <saschafricke@frg-vfx.com>
2016-06-15 12:23:45 +0200

    unused test

--- source/plugins/lsystem/__init__.py
+++ source/plugins/lsystem/__init__.py
@@ -1,4 +1,40 @@
 import MT
+from PyQt4.QtGui import *
+
+class SyncInputsWidget(MT.pytypes.CustomNodeWidget):
+    name = "Add"
+    
+    def __init__(self, node, parent):
+        MT.pytypes.CustomNodeWidget.__init__(self, node, parent)
+
+        self.setLayout(QHBoxLayout())
+
+        button = QPushButton("sync")
+
+        self.layout().setMargin(0)
+        self.layout().setSpacing(0)
+        self.layout().addWidget(button)
+
+        button.clicked.connect(self.sync)
+
+    def sync(self):
+        axioms = []
+        axiom_array_node = None
+        transform_array_node = None
+        for child in self.node.insockets[1].childNodes:
+            if child.type == "ARRAYNODE":
+                axiom_array_node = child
+                axioms = [s.value for s in child.insockets if s.value is not None
+                          and s.type != "VARIABLE"]
+        for child in self.node.insockets[2].childNodes:
+            if child.type == "ARRAYNODE":
+                transform_array_node = child
+
+        print(axioms)
+        for a in axioms:
+
+            if not a in [s.name for s in transform_array_node.insockets]:
+                transform_array_node.addInSocket(a, "TRANSFORMABLE")
 
 class LSystemNodeDecorator(MT.pytypes.NodeDecorator):
     label = "LSystem"
@@ -9,5 +45,7 @@ class LSystemNodeDecorator(MT.pytypes.NodeDecorator):
         ("Transformations", "LIST:TRANSFORMABLE")
     ]
     outsockets = [("Skeleton", "TRANSFORMABLE")]
+    #customwidget = SyncInputsWidget
 
 MT.registerNode(LSystemNodeDecorator)
+
