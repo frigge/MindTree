978f7fc0423a02b73c9113decefe1d3986bc5b49
Sascha Fricke <frigge@frg-vfx.com>
2016-06-17 22:10:07 +0200

    lsystem logic (untested)

--- source/processors/lsystem.cpp
+++ source/processors/lsystem.cpp
@@ -6,6 +6,56 @@ using namespace MindTree;
 
 void run(DataCache *cache)
 {
+    std::string axiom = cache->getData(0).getData<std::string>();
+    auto rawrules = cache->getData(1).getData<std::vector<std::string>>();
+    auto joints = cache->getData(2).getData<std::vector<JointPtr>>();
+    auto iterations = cache->getData(3).getData<int>();
+
+    std::unordered_map<char, std::string> rules;
+    for(auto rule : rawrules) {
+        rules[rule[0]] = rule.substr(1, std::string::npos);
+    }
+
+    std::string expanded = axiom;
+    for (int i = 0; i < iterations; ++i) {
+        std::string newexpanded;
+        for (char c : expanded) {
+            if (rules.find(c) != rules.end()) {
+                newexpanded += "[" + rules[c] + "]";
+            }
+            else {
+                newexpanded += c;
+            }
+        }
+        expanded = newexpanded;
+    }
+
+    std::unordered_map<char, JointPtr> joint_map;
+    for(const auto j : joints)
+        joint_map[j->getName()[0]] = j; 
+
+    Joint *parent{nullptr};
+    JointPtr j, root;
+    for (const char c : expanded) {
+        if(c == '{')
+            parent = j.get();
+        else if(c == '}')
+            parent = dynamic_cast<Joint*>(j->getParent());
+        else if (joint_map.find(c) == joint_map.end())
+            continue;
+        else {
+            j = std::dynamic_pointer_cast<Joint>(joint_map[c]->clone());
+            if(!root) {
+                root = j;
+                parent = root.get();
+                continue;
+            }
+
+            parent->addChild(j);
+        }
+    }
+
+    cache->pushData(root);
 }
 
 extern "C" {
